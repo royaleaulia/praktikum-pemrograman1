35f9732738fd34d40d26cd002de5beb2754bd7e4
Muhammad Aulia Akbar <101610494+royaleaulia@users.noreply.github.com>
2023-12-08 22:51:20 +0800

    Update PRAK601-2010817210023-MuhammadAuliaAkbar.py

--- Praktikum6/Bahasa Python/PRAK601-2010817210023-MuhammadAuliaAkbar.py
+++ Praktikum6/Bahasa Python/PRAK601-2010817210023-MuhammadAuliaAkbar.py
@@ -1,13 +1,13 @@
 # Meminta input ukuran matriks
-baris = int(input("Masukkan jumlah baris: "))
-kolom = int(input("Masukkan jumlah kolom: "))
+baris = int(input("Baris: "))
+kolom = int(input("Kolom: "))
 
 # Membuat matriks kosong
 matriks = []
 
 # Meminta input elemen matriks
 for i in range(baris):
-    baris_matriks = [int(input(f"Masukkan elemen baris {i + 1}, kolom {j + 1}: ")) for j in range(kolom)]
+    baris_matriks = [int(input(f"")) for j in range(kolom)]
     matriks.append(baris_matriks)
 
 # Menampilkan matriks
